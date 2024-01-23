b20026300eb1ba83171e40ee362f0b7365311819
softdeveloper23 <brannongarrett77@gmail.com>
2024-01-23 21:44:22 +0100

    Created js file and getAllPlanets function

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,6 @@ const planetsRouter = require('./routes/planets/planets.router');
 const app = express();
 
 app.use(express.json());
-
 app.use(planetsRouter);
 
 module.exports = app;
---
+++ server/src/routes/planets/planets.controller.js
@@ -0,0 +1,5 @@
+const planets = [];
+
+function getAllPlanets(req, res) {
+    res.status(200).json(planets);
+}
