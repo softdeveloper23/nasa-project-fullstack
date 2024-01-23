5f90784e9525cfe4f93bcfa129bc14a968e843ed
softdeveloper23 <brannongarrett77@gmail.com>
2024-01-23 21:28:16 +0100

    Created app.js and moved express code there

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,10 @@
 const http = require('http');
 
+const app = require('./app');
+
 const PORT = process.env.PORT || 8000; // Checks if there is a port specified in the environment or defaults to PORT 8000
 
-const server = http.createServer(/*SOMETHING*/);
+const server = http.createServer(app);
 
 server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
