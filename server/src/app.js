e3e5fbbfc6811da3c361d6ff9312500e60bd6c6e
softdeveloper23 <brannongarrett77@gmail.com>
2024-01-24 14:51:20 +0100

    Successfully added the deploy script

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 
@@ -8,8 +9,14 @@ const app = express(); // This will create an Express application
 
 app.use(cors({
     origin: 'http://localhost:3000', // app.use(cors()); // This will allow all requests from all origins
-})); 
+}));
+
 app.use(express.json()); // This will parse the JSON data from the request body into a JS object so that we can access it in our code
+app.use(express.static(path.join(__dirname, '..', 'public'))); // This will serve the static files from the public directory
+
 app.use(planetsRouter); // This will use the planets router we created in the previous step
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
