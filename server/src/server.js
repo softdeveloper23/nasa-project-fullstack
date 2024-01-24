436aeb8ac99b35407eee752a03f9037326328535
softdeveloper23 <brannongarrett77@gmail.com>
2024-01-24 09:49:15 +0100

    Created an async function in server.js to await loadPlanetsData.

--- server/src/server.js
+++ server/src/server.js
@@ -2,15 +2,24 @@ const http = require('http');
 
 const app = require('./app');
 
+const { loadPlanetsData } = require('./models/planets.model')
+
 // Checks if there is a port specified in the environment or defaults to PORT 8000
 const PORT = process.env.PORT || 8000;
 
 // Creates a server that will listen to requests on the specified PORT
 const server = http.createServer(app);
 
-// Starts the server
-server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`);
-});
+async function startServer() {
+    await loadPlanetsData();
+
+    // Starts the server
+    server.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}...`);
+    });
+}
+
+startServer();
+
 
 
