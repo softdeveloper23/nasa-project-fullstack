0633dcb9e419c3132c24c7e3678037d70f8e3b71
softdeveloper23 <brannongarrett77@gmail.com>
2024-01-24 09:19:37 +0100

    Created a data folder and added Kepler data csv

--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const app = require('./app');
 
 // Checks if there is a port specified in the environment or defaults to PORT 8000
-const PORT = process.env.PORT || 8000; 
+const PORT = process.env.PORT || 8000;
 
 // Creates a server that will listen to requests on the specified PORT
 const server = http.createServer(app);
