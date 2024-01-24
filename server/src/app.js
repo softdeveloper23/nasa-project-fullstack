e4bf980c9a078ee95b44f4fef8a3a70243dd087a
softdeveloper23 <brannongarrett77@gmail.com>
2024-01-24 15:05:53 +0100

    Installed and implemented npm morgan for request logging

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router'); // This will import the planets router we created in the previous step
 
@@ -10,6 +11,8 @@ const app = express(); // This will create an Express application
 app.use(cors({
     origin: 'http://localhost:3000', // app.use(cors()); // This will allow all requests from all origins
 }));
+
+app.use(morgan('combined')); // To log requests
 
 app.use(express.json()); // This will parse the JSON data from the request body into a JS object so that we can access it in our code
 app.use(express.static(path.join(__dirname, '..', 'public'))); // This will serve the static files from the public directory
