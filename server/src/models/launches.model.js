d048814674e4b3c227f7433aa7ad43b5002b9264
softdeveloper23 <brannongarrett77@gmail.com>
2024-01-25 16:48:13 +0100

    Created abortLaunchById and added code to change upcoming and success properties in launches object to false

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -36,8 +36,15 @@ function addNewLaunch(launch) {
     );
 }
 
+function abortLaunchById(launchId) {
+    const aborted = launches.get(launchId);
+    aborted.upcoming = false;
+    aborted.success = false;
+}
+
 module.exports = {
     existLaunchWithId,
     getAllLaunches,
     addNewLaunch,
+    abortLaunchById,
 };
