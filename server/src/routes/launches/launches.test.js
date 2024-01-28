212828329b9904fea8aaef678097c0a6305f97ca
softdeveloper23 <brannongarrett77@gmail.com>
2024-01-28 20:50:26 +0100

    Load planets data in tests

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,14 +1,16 @@
 const request = require('supertest');
 const app = require('../../app');
 const {mongoConnect, mongoDisconnect,} = require('../../services/mongo');
+const { loadPlanetsData } = require('../../models/planets.model');
 
 describe('launches API', () => {
     beforeAll(async () => {
         await mongoConnect();
+        await loadPlanetsData();
     });
 
     afterAll(async () => {
-        await mongoDisconnect;
+        await mongoDisconnect();
     });
     describe('Test GET /launches', () => {
         test('It should respond with 200 success', async () => {
